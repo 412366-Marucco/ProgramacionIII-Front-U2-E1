518f92a503c5ba76029ac74ea61467906eaa2b47
412366-Marucco <412366@tecnicatura.frc.utn.edu.ar>
2025-04-11 10:21:26 -0300

    Refactored app.js.

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,17 @@
 const formulario = document.getElementById('formulario-contacto');
 
-formulario.addEventListener('submit', (e) => {
+function manejarEnvioFormulario(e) {
     e.preventDefault();
+
     const nombre = document.getElementById('nombre').value;
     const email = document.getElementById('email').value;
     const mensaje = document.getElementById('mensaje').value;
-
 
     const datos = {
         nombre,
         email,
         mensaje
-    }
+    };
 
     console.log(datos);
 
@@ -19,9 +19,9 @@ formulario.addEventListener('submit', (e) => {
 
     const mensajeGracias = document.createElement("p");
     mensajeGracias.textContent = "¡Gracias por tu mensaje!";
-    mensajeGracias.className = "text-green-600 text-lg mt-4";
+    mensajeGracias.classList.add("text-green-600", "text-lg", "mt-4");
     formulario.parentNode.appendChild(mensajeGracias);
 
-})
+}
 
-
+formulario.addEventListener('submit', manejarEnvioFormulario);
